333f1179b365b6cc942aea77fcb7f032a51bfe5f
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2025-03-20 14:15:32 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,7 @@
                     @yield('content')
                 </div><!-- /.container-fluid -->
             </section>
-            <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+            {{-- <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script> --}}
     <!-- JavaScript Datatables -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
